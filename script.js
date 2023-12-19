dfb2060fc976c8b95adc9adbb595b1e4ca197696
Vincent <77495110+syscodex@users.noreply.github.com>
2023-12-19 20:13:07 +0800

    Fix reset button

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+let color = "black";
+
+let click = false;
 document.addEventListener("DOMContentLoaded", function(){
     //default size
     createBoard(16);
 
-    let color = "black";
+     
+    
+    document.querySelector("body").addEventListener("click", function(e){
+        if (e.target.tagName != "BUTTON") {
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click) {
+                draw.innerHTML = "Now you can draw";
+        }
+        else {
+            draw.innerHTML = "You are not allowed to draw";
+        }
+        }
+    })
     
 
     
@@ -56,13 +72,15 @@ function getSize () {
 //
 
 function colorDiv () {
-   if (color == "random") {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-   }
-   else {
-    this.style.backgroundColor = 'black';
-   }
-}
+   if(click) {
+    if (color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else {
+        this.style.backgroundColor = 'black';
+    }
+    }
+    }
 
 function setColor(colorChoice) {
  color = colorChoice;
